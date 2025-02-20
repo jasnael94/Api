de5afe5aaa06389c5e362d17d708304397dec421
Jason <jasnael94@gmail.com>
2025-02-20 15:15:01 +0100

    update

--- src/Controller/MailjetController.php
+++ src/Controller/MailjetController.php
@@ -31,7 +31,7 @@ class MailjetController extends AbstractController
 
         // Envoi de l'email
         $emailSent = $this->mailjetService->sendEmailFromTemplate(
-            'example@gmail.com', // Destinataire
+            'jasnael94@icloud.com', // Destinataire
             'Member1', // Nom du destinataire
             $templateId, // ID du template Mailjet
             $variables // Variables dynamiques à insérer
---
+++ src/Entity/User.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\UX\Turbo\Attribute\Broadcast;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[Broadcast]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Mail = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $password = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->Name;
+    }
+
+    public function setName(string $Name): static
+    {
+        $this->Name = $Name;
+
+        return $this;
+    }
+
+    public function getMail(): ?string
+    {
+        return $this->Mail;
+    }
+
+    public function setMail(string $Mail): static
+    {
+        $this->Mail = $Mail;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+}
